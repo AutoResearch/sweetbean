c470aed321dee37f38be0673427f4ad3f88ee42b
Younes Strittmatter <younes_strittmatter@brown.edu>
2024-04-26 15:10:21 +0200

    Update text.py

--- examples/survey/text.py
+++ examples/survey/text.py
@@ -1,5 +1,12 @@
+"""
+After the participant is greeted with 'Welcome! We show a survey. Press SPACE to continue' until they press the Spacebar,
+they are asked free-from questions about how old they are, their gender, and their handedness.
+"""
+
 from sweetbean.sequence import Block, Experiment
 from sweetbean.stimulus import TextStimulus, TextSurveyStimulus
+
+## EVENT SEQUENCE
 
 stim_1 = TextStimulus(
     text="Welcome! We show a survey. Press SPACE to continue",
@@ -10,6 +17,10 @@ stim_2 = TextSurveyStimulus(
     prompts=["How old are you?", "What is your gender?", "What is your handedness?"]
 )
 
-trial_sequence = Block([stim_1, stim_2])
+event_sequence = [stim_1, stim_2]
+
+## BLOCK DESIGN
+
+trial_sequence = Block(event_sequence)
 experiment = Experiment([trial_sequence])
 experiment.to_html("text.html")
