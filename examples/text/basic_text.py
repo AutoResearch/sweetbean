d4e93f40b69a2bebfe4bab6f03bf6f3d7d24a393
Younes Strittmatter <younes_strittmatter@brown.edu>
2024-04-26 14:25:22 +0200

    Update basic_text.py

--- examples/text/basic_text.py
+++ examples/text/basic_text.py
@@ -1,3 +1,7 @@
+"""
+The text 'press a or b or wait' is shown in pink for 2000ms. The allowed responses are 'a' and 'b'.
+"""
+
 from sweetbean.sequence import Block, Experiment
 from sweetbean.stimulus import TextStimulus
 
