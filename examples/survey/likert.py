af890bc3c842860521328ed0cfcac39fd61eee0a
Younes Strittmatter <younes_strittmatter@brown.edu>
2024-04-26 15:02:02 +0200

    Update likert.py

--- examples/survey/likert.py
+++ examples/survey/likert.py
@@ -1,5 +1,14 @@
+"""
+A text stimulus to welcome the participant is shown until they press the Spacebar. The text says, 'Welcome! We show a survey. Press SPACE to continue'.
+This is followed by a survey with two questions that the participant is supposed to answer on a Likert scale. The first question is 'How are you feeling?' and
+the participant is supposed to rate their feeling on a scale from -2 to 2 with 5 levels. The second question is 'Do you like this tool?' on a scale from 1 to 3.
+Then, the participant is supposed to rate five animals on their cuteness from 1 to 5. The animals are a bunny, a shark, a spider, a cat, and a dog.
+"""
+
 from sweetbean.sequence import Block, Experiment
 from sweetbean.stimulus import LikertSurveyStimulus, TextStimulus
+
+## EVENT SEQUENCE
 
 stim_1 = TextStimulus(
     text="Welcome! We show a survey. Press SPACE to continue",
@@ -22,6 +31,10 @@ stim_4 = LikertSurveyStimulus.from_scale(
     prompts=["bunny", "shark", "spider", "cat", "dog"], scale=[1, 2, 3, 4, 5]
 )
 
-trial_sequence = Block([stim_1, stim_2, stim_3, stim_4])
+event_sequence = [stim_1, stim_2, stim_3, stim_4]
+
+## BLOCK DESIGN
+
+trial_sequence = Block(event_sequence)
 experiment = Experiment([trial_sequence])
 experiment.to_html("likert.html")
