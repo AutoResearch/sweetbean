5f8e3c34a9d842ba3f3d35909cb5e10130d90f02
Younes Strittmatter <younes_strittmatter@brown.edu>
2024-04-26 14:44:35 +0200

    Update basic_text.py

--- examples/text/basic_text.py
+++ examples/text/basic_text.py
@@ -5,11 +5,17 @@ The text 'press a or b or wait' is shown in pink for 2000ms. The allowed respons
 from sweetbean.sequence import Block, Experiment
 from sweetbean.stimulus import TextStimulus
 
+## THE EVENT SEQUENCE
+
 text = TextStimulus(
     duration=2000, text="press a or b or wait", color="pink", choices=["a", "b"]
 )
 
-block = Block([text])
+event_sequence = [text]
+
+## BLOCK DESIGN
+
+block = Block(event_sequence)
 experiment = Experiment([block])
 
 experiment.to_html("basic.html")
