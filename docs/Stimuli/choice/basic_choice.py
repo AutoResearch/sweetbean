ef9d1b7f8a666f3bc4f184626e6a1a3c51fc783c
Younes Strittmatter <younes_strittmatter@brown.edu>
2024-11-30 09:30:14 -0500

    feat: clean up old version

--- docs/Stimuli/choice/basic_choice.py
+++
@@ -1,8 +0,0 @@
-from sweetbean.sequence import Block, Experiment
-from sweetbean.stimulus import ChoiceStimulus
-
-stim_1 = ChoiceStimulus()
-
-trial_sequence = Block([stim_1])
-experiment = Experiment([trial_sequence])
-experiment.to_html("basic.html")
